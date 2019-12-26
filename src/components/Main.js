e9c881ab5b0ad169d9a3730c1de77882f1f121a9
Robbie08 <roberinho08@gmail.com>
2019-12-26 10:07:20 -0800

    Consumed API and updated UI with API content

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,28 +4,19 @@ import Wall from './Wall'
 
 
 const title = "Random Number Facts"
-class Main extends Component {
-    constructor(){
-        super()
-        this.state = {
-            NumberFact: {
-                number: 8,
-                fact: "8 is the number of principles of Yong in Chinese calligraphy."
-            }
-            
-        }
-    }
 
+class Main extends Component {
     render() {
         return(
             <div>
                 <h1>{title}</h1>
                 <div>
-                    <Wall data={this.state.NumberFact}></Wall>
+                    <Wall></Wall>
                 </div>
             </div>
         )
     }
+
 }
 
 export default Main
--- src/components/Wall.js
+++ src/components/Wall.js
@@ -1,26 +1,70 @@
-import React from 'react'
+import React, { Component } from 'react'
 import {Card} from 'react-bootstrap'
 import {Button} from 'react-bootstrap'
 import {Form} from 'react-bootstrap'
+import { render } from 'react-dom'
+
+const API = "http://numbersapi.com/"
 
 // this function will accept and object as prop to populate the card
-function Wall(props){
-    const data = props.data // destructuring data passed as an object
-    return(
-        <div>
-           <Card className="text-center">
-           
-            <Card.Header></Card.Header>
-            <Card.Body>
-                <Card.Title>The number: {data.number}</Card.Title>
-                <Card.Text className="text-primary">{data.fact}</Card.Text>
-                <Form.Control size="text" type="text" placeholder="Enter a number to find out an intersting fact about it" />
-                <br/>
-                <Button variant="primary">Fetch Fact</Button>
-            </Card.Body>
-            </Card>
-        </div>
-    )
+class Wall extends Component {
+    state = {
+        number: "",
+        numberView: "",
+        fact:""
+    }
+
+    // handle the user data from the form
+    getUserData = (event) => {
+        // we have to update our state
+        this.setState({
+                number: event.target.value
+            }
+        );  
+    }
+
+    
+
+    // handle the user data when the button is clicked
+    onFetchedState = async () => {
+        // concat number to api link
+        const URL = `${API}${this.state.number}`
+    
+        // use that for fetch
+        try {
+            const response = await fetch(URL);
+            const text = await response.text()
+            console.log(text)
+    
+            // set state string for number fact
+            this.setState({
+                    fact: text,
+                    numberView: this.state.number,
+                    
+            })
+        }
+        catch(E) {
+            console.log(E); // output error
+        }
+    }
+
+    render(){
+        return(
+            <div>
+               <Card className="text-center">
+                <Card.Header></Card.Header>
+                <Card.Body>
+                    <Card.Title>The number: {this.state.numberView}</Card.Title>
+                    <Card.Text className="text-primary">{this.state.fact}</Card.Text>
+                    <Form.Control onChange={this.getUserData} name="Number" size="text" type="text" placeholder="Enter a number to find out an intersting fact about it" />
+                    <br/>
+                    <Button onClick={this.onFetchedState} variant="primary">Fetch Fact</Button>
+                </Card.Body>
+                </Card>
+            </div>
+        )
+
+    }
 
 }
 
