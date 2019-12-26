294bcfbb7ff56a3c9f69964c7ca9b6822e033480
Robbie08 <roberinho08@gmail.com>
2019-12-26 13:54:02 -0800

    Cleaned up and completed current front end work

--- src/components/Main.js
+++ src/components/Main.js
@@ -13,6 +13,14 @@ class Main extends Component {
                 <div>
                     <Wall></Wall>
                 </div>
+                <footer>
+                    <div>
+                        <h2>by Robert Ortiz</h2>
+                    </div>
+                    <div>
+                        <h3>with the help of <a href="http://numbersapi.com/">Numbers API</a></h3>
+                    </div>
+                </footer>
             </div>
         )
     }
