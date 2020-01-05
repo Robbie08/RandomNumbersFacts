eee85cc078555fa74f7b43f2361a34f7aa7e2874
jcan1995 <cantero.joshua@gmail.com>
2020-01-04 16:32:36 -0800

    cleanup

--- src/components/Wall.js
+++ src/components/Wall.js
@@ -1,53 +1,49 @@
-import React, { Component } from 'react'
-import {Card} from 'react-bootstrap'
-import {Button} from 'react-bootstrap'
-import {Form} from 'react-bootstrap'
-import { render } from 'react-dom'
+import React, { Component } from 'react';
+import { Card } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
+import { Form } from 'react-bootstrap';
 
-const API = "http://numbersapi.com/"  // API URL
+const API = 'http://numbersapi.com/';  // API URL
 
 // this function will accept and object as prop to populate the card
 class Wall extends Component {
     state = {
-        number: "random",
-        numberView: "random",
-        fact:"12 is the number of keys in any standard digital telephone (1 through 9, 0, * and #)."
-    }
+        number: 'random',
+        numberView: 'random',
+        fact: '12 is the number of keys in any standard digital telephone (1 through 9, 0, * and #).'
+    };
 
     // handle the user data from the form
-    getUserData = (event) => {
+    getNumberInput = (event) => {
         // we have to update our state
-        this.setState({
-                number: event.target.value
-            }
-        );  
+        this.setState({ number: event.target.value });  
     }
 
     // handle the user data when the button is clicked
-    onFetchedState = async (e) => {
+    fetchFact = async () => {
+        
+        const numberInput = this.state.number;
         // Initialize the correct URL
-        // if the button has 
-        let URL = (e !== undefined) ? `${API}${this.state.number}` :  `${API}${"random"}`
+        // If the button has 
+        const URL = numberInput ? `${API}${numberInput}` :  `${API}random`;
 
         // use that for fetch
         try {
             const response = await fetch(URL);
-            const text = await response.text()
+            const text = await response.text();
     
             // set state string for number fact
             this.setState({
                     fact: text,
-                    numberView: this.state.number,
-                    
-            })
-        }
-        catch(E) {
+                    numberView: numberInput
+            });
+        } catch(E) {
             console.log(E); // output error
         }
     }
     
-    async componentDidMount (){
-        this.onFetchedState(); // call the onFetchedState to update UI
+    async componentDidMount () {
+        this.fetchFact(); // call the fetchFact to update UI
     }
 
     render(){
@@ -58,9 +54,9 @@ class Wall extends Component {
                 <Card.Body>
                     <Card.Title>The number: {this.state.numberView}</Card.Title>
                     <Card.Text className="text-primary">{this.state.fact}</Card.Text>
-                    <Form.Control onChange={this.getUserData} name="Number" size="text" type="text" placeholder="Enter a number to find out an intersting fact about it" />
+                    <Form.Control onChange={this.getNumberInput} name="Number" size="text" type="text" placeholder="Enter a number to find out an intersting fact about it" />
                     <br/>
-                    <Button onClick={this.onFetchedState} id="btnClicked" variant="primary">Fetch Fact</Button>
+                    <Button onClick={this.fetchFact} id="btnClicked" variant="primary">Fetch Fact</Button>
                 </Card.Body>
                 </Card>
             </div>
